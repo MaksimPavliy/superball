0cd2c0e7c25b2c9a72771cf10219b6a0b68a37c5
Anton Pendiur <antonetalon@gmail.com>
2020-10-14 18:43:14 +0300

    fgt awakened and fixes

--- Assets/HC/Balance/HСSkinsConfig.cs
+++ Assets/HC/Balance/HСSkinsConfig.cs
@@ -1,6 +1,6 @@
 ﻿using FriendsGamesTools.DebugTools;
 
-public class HСSkinsConfig : BalanceSettings<HСGeneralConfig>
+public class HСSkinsConfig : BalanceSettings<HСSkinsConfig>
 {
     public double basePrice = 100;
     public double priceIncrement = 100;
