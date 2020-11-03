9b2f4200eb1e52ff5023ebb6fd3837aa8da0a53a
Anton Pendiur <antonetalon@gmail.com>
2020-11-03 20:39:54 +0200

    new fgt modules

--- Assets/HC/Levels/HCLocationsController.cs
+++ Assets/HC/Levels/HCLocationsController.cs
@@ -3,7 +3,8 @@ using Unity.Entities;
 
 namespace HC
 {
-    public class HCLocationsController : LevelBasedController {
+    public class HCLocationsController : LevelBasedController
+    {
         protected override (bool win, bool lose) CheckWinLose() => (false, false);
         HСMoneyConfig moneyConfig => HСMoneyConfig.instance;
         public override int levelWinMoney => moneyConfig.levelWinMoney;
