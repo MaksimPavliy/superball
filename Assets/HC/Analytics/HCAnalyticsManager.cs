26fb03c58186e35c8f110791a077774ceea60d60
art leving <336382@gmail.com>
2020-11-17 12:52:03 +0200

    HC fix - a lot of files

---
+++ Assets/HC/Analytics/HCAnalyticsManager.cs
@@ -0,0 +1,27 @@
+﻿using FriendsGamesTools.Analytics;
+
+namespace HC
+{
+    public class HCAnalyticsManager
+    {
+        static void Send(string eventName, params (string key, object value)[] parameters) => AnalyticsManager.Send(eventName, parameters);
+
+        public static void LevelStart(string levelNumber)
+        {
+            Send("Level started", ("level number", levelNumber));
+        }
+
+        public static void LevelFailed(string levelNumber)
+        {
+            Send("Level failed", ("level number", levelNumber));
+        }
+
+        public static void LevelFinish(string levelNumber)
+        {
+            Send("Level finished", ("level number", levelNumber));
+        }
+
+        public static void SkinBought(int ind) =>
+            Send("Skin bought", ("skin index", ind));
+    }
+}
