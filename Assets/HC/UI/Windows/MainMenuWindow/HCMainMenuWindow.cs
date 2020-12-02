20dc029eea72022356f35f1eeedf4ea5eaac5f93
Artem Danich <gothmogx@gmail.com>
2020-12-02 15:38:56 +0200

    created hc ui prefabs

---
+++ Assets/HC/UI/Windows/MainMenuWindow/HCMainMenuWindow.cs
@@ -0,0 +1,11 @@
+﻿using FriendsGamesTools.ECSGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace HC
+{
+    public class HCMainMenuWindow : MainMenuWindow
+    {
+
+    }
+}
