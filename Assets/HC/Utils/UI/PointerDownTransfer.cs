2771b90cf7786f9e50ff3121c464698fad15e5c9
Artem Danich <gothmogx@gmail.com>
2021-03-03 13:13:55 +0200

    few utils added

---
+++ Assets/HC/Utils/UI/PointerDownTransfer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointerDownTransfer : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private GameObject playButton;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ExecuteEvents.Execute(playButton.gameObject, eventData, ExecuteEvents.pointerDownHandler);
+    }
+}
