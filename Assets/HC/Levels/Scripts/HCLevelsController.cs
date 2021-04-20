47870ff6ba7822fd2ed3310ea9c7248a0e92404a
Artem Danich <gothmogx@gmail.com>
2021-04-20 09:53:55 +0300

    level controller fix

--- Assets/HC/Levels/Scripts/HCLevelsController.cs
+++ Assets/HC/Levels/Scripts/HCLevelsController.cs
@@ -10,7 +10,7 @@ namespace HC
         public override int levelWinMoney => moneyConfig.levelWinMoneyBase + currLocationInd * moneyConfig.levelWinMoneyCoef;
         public override float levelWinX3Chance => moneyConfig.levelWinX3Chance;
         public override int winStarsCount => Utils.Random(0, 1f) <= 0.7f ? 3 : 2;
-
+        public bool LastGameIsWin { private set; get; } = false;
         public override void RestartLocation()
         {
             base.RestartLocation();
@@ -28,14 +28,14 @@ namespace HC
         protected override void OnWin()
         {
             base.OnWin();
-
+            LastGameIsWin = true;
             HCAnalyticsManager.LevelFinish(currLocationInd.ToString());
         }
 
         protected override void OnLose()
         {
             base.OnLose();
-
+            LastGameIsWin = false;
             HCAnalyticsManager.LevelFailed(currLocationInd.ToString());
         }
     }
