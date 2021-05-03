1ed8c0bcde9da1582986d4c4373a1de2c5d06547
Artem Danich <gothmogx@gmail.com>
2021-05-03 10:47:15 +0300

    blocker added

---
+++ Assets/HC/UI/Ads/AdButtonBlocker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace HcUtils
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class AdButtonBlocker : MonoBehaviour
+    {
+        [SerializeField] private GameObject notAvailableParent;
+        [SerializeField] private CanvasGroup canvasGroup;
+        protected virtual bool RewardedAvailable => true;
+        protected virtual bool GameLoaded => true;
+
+        private void Awake()
+        {
+           if(!canvasGroup) canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        void UpdateAvailable()
+        {
+            bool available = RewardedAvailable;
+            if (canvasGroup) canvasGroup.interactable = available;
+            if (notAvailableParent) notAvailableParent.gameObject.SetActive(!available);
+        }
+
+        private void Update()
+        {
+            if (!GameLoaded) return;
+            
+            UpdateAvailable();
+        }
+    }
+}
---
+++ Assets/HC/UI/Ads/HCAdButtonBlocker.cs
@@ -0,0 +1,10 @@
+﻿using HC;
+
+namespace HcUtils
+{
+    public class HCAdButtonBlocker : AdButtonBlocker
+    {
+        protected override bool GameLoaded => HCAdsManager.instance;
+        protected override bool RewardedAvailable => HCAdsManager.instance.rewarded.available;
+    }
+}
