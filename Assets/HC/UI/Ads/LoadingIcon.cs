8d7e6fdd514a92d7ce734d9ba89ec5e0e854010f
Artem Danich <gothmogx@gmail.com>
2021-05-03 10:47:00 +0300

    ui sprites update

---
+++ Assets/HC/UI/Ads/LoadingIcon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DTL.UI
+{
+    public class LoadingIcon : MonoBehaviour
+    {
+        private Image image;
+        private float fillValue = 0;
+        Quaternion startRotation;
+        Quaternion endRotation;
+        private float lastFillValue = 0;
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+        }
+        private void Update()
+        {
+            if (!image) return;
+            fillValue = Mathf.PingPong(Time.time, 1f);
+            if (fillValue < lastFillValue)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Lerp(0, 360, fillValue)));
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+                  lastFillValue = fillValue;
+            image.fillAmount = fillValue;
+        }
+    }
+}
