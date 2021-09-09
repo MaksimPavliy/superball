d74d98489d6aa2875aa3135db42b44a9c00a671f
Artem Danich <gothmogx@gmail.com>
2021-09-09 16:42:08 +0300

    tweak pointer down transfer

--- Assets/HC/Utils/UI/PointerDownTransfer.cs
+++ Assets/HC/Utils/UI/PointerDownTransfer.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PointerDownTransfer : MonoBehaviour, IPointerDownHandler
 {
     [SerializeField]
-    private GameObject playButton;
-
+    private Button playButton;
+    private void Awake()
+    {
+        if (playButton.targetGraphic)
+        {
+            playButton.targetGraphic.raycastTarget = false;
+        }
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
         ExecuteEvents.Execute(playButton.gameObject, eventData, ExecuteEvents.pointerDownHandler);
